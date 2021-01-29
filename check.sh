b51b01bd8d8c15a5b6ce0d29c420965bd683a06c
Michael Leonhard <michael@leonhardllc.com>
2021-01-29 14:33:26 -0800

    Don't run `cargo geiger` on nightly.

--- check.sh
+++ check.sh
@@ -5,8 +5,13 @@
 
 check_cargo_readme() {
   cargo readme >Readme.md.tmp
-  ls -alF $CI_PROJECT_DIR
-  cargo geiger --update-readme --readme-path Readme.md.tmp --output-format GitHubMarkdown
+  ls -alF "$CI_PROJECT_DIR" || true
+  # Once cargo-geiger-serde builds on nightly,
+  # change this to always run `cargo geiger`.
+  # https://github.com/rust-secure-code/cargo-geiger/issues/181
+  if [ "$CI_JOB_NAME" != "rust-nightly" ]; then
+    cargo geiger --update-readme --readme-path Readme.md.tmp --output-format GitHubMarkdown
+  fi
   diff Readme.md Readme.md.tmp || (
     echo "Readme.md is stale" >&2
     exit 1
@@ -16,22 +21,22 @@ check_cargo_readme() {
 }
 
 check() {
-  ls -alF $CI_PROJECT_DIR
+  ls -alF "$CI_PROJECT_DIR" || true
   time cargo check --verbose
-  ls -alF $CI_PROJECT_DIR
+  ls -alF "$CI_PROJECT_DIR" || true
   time cargo build --verbose
-  ls -alF $CI_PROJECT_DIR
+  ls -alF "$CI_PROJECT_DIR" || true
   time cargo test --verbose
-  ls -alF $CI_PROJECT_DIR
+  ls -alF "$CI_PROJECT_DIR" || true
   time cargo fmt --all -- --check
-  ls -alF $CI_PROJECT_DIR
+  ls -alF "$CI_PROJECT_DIR" || true
   time cargo clippy --all-targets --all-features -- -D clippy::pedantic
-  ls -alF $CI_PROJECT_DIR
+  ls -alF "$CI_PROJECT_DIR" || true
   #time check_cargo_readme
-  ls -alF $CI_PROJECT_DIR
+  ls -alF "$CI_PROJECT_DIR" || true
   time cargo publish --dry-run "$@"
   echo "$0 finished"
-  ls -alF $CI_PROJECT_DIR
+  ls -alF "$CI_PROJECT_DIR" || true
 }
 set -e
 set -x
