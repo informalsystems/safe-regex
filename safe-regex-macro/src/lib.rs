bd61ccb5bd698fc3c151112a4565fa9905a515d5
Michael Leonhard <michael@leonhardllc.com>
2021-02-01 19:02:59 -0800

    Added more tests and fixed bugs.

--- safe-regex-macro/src/lib.rs
+++ safe-regex-macro/src/lib.rs
@@ -50,9 +50,26 @@ enum NonFinal {
     HexEscape0,
     HexEscape1(u8),
     OpenGroup,
+    OpenOr(Vec<Final>),
     RepeatMin(Box<Final>, String),
     RepeatMax(Box<Final>, String, String),
-    OpenOr(Vec<Final>),
+}
+impl NonFinal {
+    pub fn reason(&self) -> String {
+        match self {
+            NonFinal::Escape => "incomplete escape sequence `\\`".to_string(),
+            NonFinal::HexEscape0 => "incomplete escape sequence `\\x`".to_string(),
+            NonFinal::HexEscape1(d) => {
+                format!("incomplete escape sequence `\\x{}`", escape_ascii([*d]))
+            }
+            NonFinal::OpenGroup => "group missing closing `)`".to_string(),
+            NonFinal::OpenOr(_) => "missing item after bar `|`".to_string(),
+            NonFinal::RepeatMin(_, s) => format!("incomplete repeat mark `{{{}`", s),
+            NonFinal::RepeatMax(_, min, max) => {
+                format!("incomplete repeat mark `{{{},{}`", min, max)
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialOrd, PartialEq)]
@@ -131,10 +148,15 @@ fn parse(mut data: &[u8]) -> Result<Final, String> {
                 opt_top.map(|top| stack.push(top));
                 stack.push(FinalNode(AnyByte))
             }
+            (_, Some(FinalNode(Or(items))), b'|') => stack.push(NonFinalNode(OpenOr(items))),
             (_, Some(FinalNode(item)), b'|') => stack.push(NonFinalNode(OpenOr(vec![item]))),
+            (_, None, b'|') => return Err("missing item before bar `|`".to_string()),
             (_, opt_top, b'(') => {
                 opt_top.map(|top| stack.push(top));
                 stack.push(NonFinalNode(OpenGroup));
+            }
+            (Some(NonFinalNode(OpenGroup)), Some(NonFinalNode(non_final)), b')') => {
+                return Err(non_final.reason())
             }
             (Some(NonFinalNode(OpenGroup)), Some(FinalNode(item)), b')') => {
                 stack.pop();
@@ -151,7 +173,7 @@ fn parse(mut data: &[u8]) -> Result<Final, String> {
             let top = stack.pop().unwrap();
             match (stack.last_mut(), top) {
                 (_, NonFinalNode(non_final)) => stack.push(NonFinalNode(non_final)),
-                (Some(NonFinalNode(OpenOr(items))), FinalNode(item)) => {
+                (Some(NonFinalNode(OpenOr(_))), FinalNode(item)) => {
                     if let Some(NonFinalNode(OpenOr(mut items))) = stack.pop() {
                         items.push(item);
                         stack.push(FinalNode(Or(items)))
@@ -182,25 +204,10 @@ fn parse(mut data: &[u8]) -> Result<Final, String> {
             }
         }
     }
+    println!("stack {:?}", stack);
     for node in stack.iter().rev() {
-        match node {
-            NonFinalNode(OpenGroup) => return Err("group missing closing `)`".to_string()),
-            NonFinalNode(Escape) => return Err("incomplete escape sequence `\\`".to_string()),
-            NonFinalNode(HexEscape0) => return Err("incomplete escape sequence `\\x`".to_string()),
-            NonFinalNode(HexEscape1(d)) => {
-                return Err(format!(
-                    "incomplete escape sequence `\\x{}`",
-                    escape_ascii([*d])
-                ))
-            }
-            NonFinalNode(RepeatMin(_, s)) => {
-                return Err(format!("incomplete repeat mark `{{{}`", s))
-            }
-            NonFinalNode(RepeatMax(_, min, max)) => {
-                return Err(format!("incomplete repeat mark `{{{},{}`", min, max))
-            }
-            NonFinalNode(OpenOr(_)) => return Err("missing item after bar `|`".to_string()),
-            FinalNode(_) => {}
+        if let NonFinalNode(non_final) = node {
+            return Err(non_final.reason());
         }
     }
     assert_eq!(1, stack.len());
@@ -215,14 +222,19 @@ fn parse(mut data: &[u8]) -> Result<Final, String> {
 #[test]
 fn test_parse() {
     use Final::{AnyByte, Byte, Group, Or, Repeat, Seq};
-    use Node::{FinalNode, NonFinalNode};
-    use NonFinal::{Escape, HexEscape0, HexEscape1, OpenGroup, OpenOr, RepeatMax, RepeatMin};
     assert_eq!(Ok(Seq(Vec::new())), parse(br""));
     assert_eq!(Ok(Byte(b'a')), parse(br"a"));
     assert_eq!(
         Ok(Seq(vec![Byte(b'a'), Byte(b'b'), Byte(b'c')])),
         parse(br"abc")
     );
+    assert_eq!(Ok(AnyByte), parse(br"."));
+}
+
+#[cfg(test)]
+#[test]
+fn test_parse_escapes() {
+    use Final::Byte;
     assert_eq!(
         Err(r"incomplete escape sequence `\`".to_string()),
         parse(br"\")
@@ -233,6 +245,11 @@ fn test_parse() {
     );
     // Rust byte escapes
     // https://doc.rust-lang.org/reference/tokens.html#byte-escapes
+    assert_eq!(Ok(Byte(b'\n')), parse(br"\n"));
+    assert_eq!(Ok(Byte(b'\r')), parse(br"\r"));
+    assert_eq!(Ok(Byte(b'\t')), parse(br"\t"));
+    assert_eq!(Ok(Byte(b'\\')), parse(br"\\"));
+    assert_eq!(Ok(Byte(0)), parse(br"\0"));
     assert_eq!(
         Err(r"incomplete escape sequence `\x`".to_string()),
         parse(br"\x")
@@ -262,23 +279,10 @@ fn test_parse() {
     assert_eq!(Ok(Byte(0xEF)), parse(br"\xef"));
     assert_eq!(Ok(Byte(0xEF)), parse(br"\xEF"));
     assert_eq!(Ok(Byte(0xFF)), parse(br"\xFF"));
-    assert_eq!(
-        Ok(Seq(vec![Byte(b'a'), Byte(0x00), Byte(b'b')])),
-        parse(br"a\x00b")
-    );
-    assert_eq!(
-        Ok(Seq(vec![
-            Byte(b'\n'),
-            Byte(b'\r'),
-            Byte(b'\t'),
-            Byte(b'\\'),
-            Byte(0),
-        ])),
-        parse(br"\n\r\t\\\0")
-    );
     // Rust quote escapes
-    //
-    assert_eq!(Ok(Seq(vec![Byte(b'\''), Byte(b'"')])), parse(br#"\'\""#));
+    // https://doc.rust-lang.org/reference/tokens.html#quote-escapes
+    assert_eq!(Ok(Byte(b'\'')), parse(br"\'"));
+    assert_eq!(Ok(Byte(b'"')), parse(br#"\""#));
     // Regex escapes
     assert_eq!(Ok(Byte(b'?')), parse(br"\?"));
     assert_eq!(Ok(Byte(b'+')), parse(br"\+"));
@@ -293,17 +297,452 @@ fn test_parse() {
     assert_eq!(Ok(Byte(b'}')), parse(br"\}"));
     assert_eq!(Ok(Byte(b'[')), parse(br"\["));
     assert_eq!(Ok(Byte(b']')), parse(br"\]"));
-    // Regex tokens
-    assert_eq!(Ok(Repeat(Box::new(Byte(b'a')), 0, Some(1))), parse(br"a?"));
-    assert_eq!(Ok(Repeat(Box::new(Byte(b'a')), 1, None)), parse(br"a+"));
-    assert_eq!(Ok(Repeat(Box::new(Byte(b'a')), 0, None)), parse(br"a*"));
-    assert_eq!(Ok(AnyByte), parse(br"."));
+}
+
+#[cfg(test)]
+#[test]
+fn test_parse_or() {
+    use Final::{AnyByte, Byte, Group, Or, Repeat, Seq};
+    assert_eq!(
+        Err(r"missing item before bar `|`".to_string()),
+        parse(br"|")
+    );
+    assert_eq!(
+        Err(r"missing item after bar `|`".to_string()),
+        parse(br"a|")
+    );
+    assert_eq!(
+        Err(r"missing item after bar `|`".to_string()),
+        parse(br"(a|)")
+    );
+    assert_eq!(
+        Err(r"missing item after bar `|`".to_string()),
+        parse(br"(a|bc|)d")
+    );
     assert_eq!(Ok(Or(vec![Byte(b'a'), Byte(b'b')])), parse(br"a|b"));
     assert_eq!(
-        Ok(Group(Box::new(Seq(vec![Byte(b'a'), Byte(b'b')])))),
-        parse(br"(ab)")
+        Ok(Or(vec![Byte(b'a'), Byte(b'b'), Byte(b'c')])),
+        parse(br"a|b|c")
+    );
+    assert_eq!(
+        Ok(Or(vec![
+            Seq(vec![Byte(b'a'), Byte(b'b')]),
+            Seq(vec![Byte(b'c'), Byte(b'd'), Byte(b'e')]),
+            Seq(vec![Byte(b'f'), Byte(b'g')])
+        ])),
+        parse(br"ab|cde|fg")
     );
 }
+
+// #[cfg(test)]
+// #[test]
+// fn test_parse_or() {
+//     use Final::{AnyByte, Byte, Group, Or, Repeat, Seq};
+//     use Node::Seq;
+//     assert_eq!(
+//         Ok(Seq(vec![Literal(b'a'), Literal(b'b')])),
+//         parse(vec![Byte(b'a'), Byte(b'b')])
+//     );
+//     use Final::{AnyByte, Byte, Group, Or, Repeat, Seq};
+//     // TODO(mleonhard) Test precedence.
+//     // Regex tokens
+//     assert_eq!(Ok(Repeat(Box::new(Byte(b'a')), 0, Some(1))), parse(br"a?"));
+//     assert_eq!(Ok(Repeat(Box::new(Byte(b'a')), 1, None)), parse(br"a+"));
+//     assert_eq!(Ok(Repeat(Box::new(Byte(b'a')), 0, None)), parse(br"a*"));
+//     assert_eq!(Ok(AnyByte), parse(br"."));
+//     assert_eq!(
+//         Ok(Group(Box::new(Seq(vec![Byte(b'a'), Byte(b'b')])))),
+//         parse(br"(ab)")
+//     );
+//
+// }
+//
+// #[cfg(test)]
+// #[test]
+// fn test_parse_class() {
+//     use Node::Class;
+//     use Token::{
+//         Bar, Byte, Caret, CloseCurly, CloseRound, CloseSquare, Dollar, Dot, OpenCurly, OpenRound,
+//         OpenSquare, Plus, QMark, Star,
+//     };
+//     assert_eq!(
+//         Ok(Class(vec![b'a'])),
+//         parse(vec![OpenSquare, Byte(b'a'), CloseSquare])
+//     );
+//     assert_eq!(
+//         Ok(Class(vec![b'a', b'b', b'c'])),
+//         parse(vec![
+//             OpenSquare,
+//             Byte(b'a'),
+//             Byte(b'b'),
+//             Byte(b'c'),
+//             CloseSquare
+//         ])
+//     );
+//     // ?+.*^$|(){}[]
+//     assert_eq!(
+//         Ok(Class(vec![
+//             b'?', b'+', b'.', b'*', b'^', b'$', b'|', b'(', b')', b'{', b'}', b'[', b']'
+//         ])),
+//         parse(vec![
+//             OpenSquare,
+//             QMark,
+//             Plus,
+//             Dot,
+//             Star,
+//             Caret,
+//             Dollar,
+//             Bar,
+//             OpenRound,
+//             CloseRound,
+//             OpenCurly,
+//             CloseCurly,
+//             OpenSquare,
+//             Byte(b']'),
+//             CloseSquare
+//         ])
+//     );
+//     assert_eq!(
+//         Err("character class ends with `-`".to_string()),
+//         parse(vec![OpenSquare, Byte(b'a'), Byte(b'-'), CloseSquare])
+//     );
+//     assert_eq!(
+//         Ok(Class(vec![b'a', b'b', b'c'])),
+//         parse(vec![
+//             OpenSquare,
+//             Byte(b'a'),
+//             Byte(b'-'),
+//             Byte(b'c'),
+//             CloseSquare
+//         ])
+//     );
+//     assert_eq!(
+//         Ok(Class(vec![b'a', b'b', b'c', b'g', b'g', b'h'])),
+//         parse(vec![
+//             OpenSquare,
+//             Byte(b'a'),
+//             Byte(b'-'),
+//             Byte(b'c'),
+//             Byte(b'g'),
+//             Byte(b'-'),
+//             Byte(b'h'),
+//             CloseSquare
+//         ])
+//     );
+//     assert_eq!(
+//         Ok(Class(vec![b'-', b'a', b'b'])),
+//         parse(vec![
+//             OpenSquare,
+//             Byte(b'-'),
+//             Byte(b'a'),
+//             Byte(b'b'),
+//             CloseSquare
+//         ])
+//     );
+// }
+//
+// #[cfg(test)]
+// #[test]
+// fn test_parse_negative_class() {
+//     use Node::NegativeClass;
+//     use Token::{
+//         Bar, Byte, Caret, CloseCurly, CloseRound, CloseSquare, Dollar, Dot, OpenCurly, OpenRound,
+//         OpenSquare, Plus, QMark, Star,
+//     };
+//     assert_eq!(
+//         Ok(NegativeClass(vec![b'a'])),
+//         parse(vec![OpenSquare, Caret, Byte(b'a'), CloseSquare])
+//     );
+//     assert_eq!(
+//         Ok(NegativeClass(vec![b'a', b'b', b'c'])),
+//         parse(vec![
+//             OpenSquare,
+//             Caret,
+//             Byte(b'a'),
+//             Byte(b'b'),
+//             Byte(b'c'),
+//             CloseSquare
+//         ])
+//     );
+//     assert_eq!(
+//         Ok(NegativeClass(vec![
+//             b'?', b'+', b'.', b'*', b'^', b'$', b'|', b'(', b')', b'{', b'}', b'[', b']'
+//         ])),
+//         parse(vec![
+//             OpenSquare,
+//             Caret,
+//             QMark,
+//             Plus,
+//             Dot,
+//             Star,
+//             Caret,
+//             Dollar,
+//             Bar,
+//             OpenRound,
+//             CloseRound,
+//             OpenCurly,
+//             CloseCurly,
+//             OpenSquare,
+//             Byte(b']'),
+//             CloseSquare
+//         ])
+//     );
+//     assert_eq!(
+//         Err("character class ends with `-`".to_string()),
+//         parse(vec![OpenSquare, Caret, Byte(b'a'), Byte(b'-'), CloseSquare])
+//     );
+//     assert_eq!(
+//         Ok(NegativeClass(vec![b'a', b'b', b'c'])),
+//         parse(vec![
+//             OpenSquare,
+//             Caret,
+//             Byte(b'a'),
+//             Byte(b'-'),
+//             Byte(b'c'),
+//             CloseSquare
+//         ])
+//     );
+//     assert_eq!(
+//         Ok(NegativeClass(vec![b'a', b'b', b'c', b'g', b'g', b'h'])),
+//         parse(vec![
+//             OpenSquare,
+//             Caret,
+//             Byte(b'a'),
+//             Byte(b'-'),
+//             Byte(b'c'),
+//             Byte(b'g'),
+//             Byte(b'-'),
+//             Byte(b'h'),
+//             CloseSquare
+//         ])
+//     );
+//     assert_eq!(
+//         Ok(NegativeClass(vec![b'-', b'a', b'b'])),
+//         parse(vec![
+//             OpenSquare,
+//             Caret,
+//             Byte(b'-'),
+//             Byte(b'a'),
+//             Byte(b'b'),
+//             CloseSquare
+//         ])
+//     );
+// }
+//
+// #[cfg(test)]
+// #[test]
+// fn test_parse_group() {
+//     use Node::{AnyByte, Empty, Group, Seq};
+//     use Token::{CloseRound, Dot, OpenRound};
+//     assert_eq!(
+//         Err("group is missing closing `)` symbol".to_string()),
+//         parse(vec![OpenRound, Dot])
+//     );
+//     assert_eq!(
+//         Ok(Group(Box::new(Empty))),
+//         parse(vec![OpenRound, CloseRound])
+//     );
+//     assert_eq!(
+//         Ok(Group(Box::new(AnyByte))),
+//         parse(vec![OpenRound, Dot, CloseRound])
+//     );
+//     assert_eq!(
+//         Ok(Group(Box::new(Group(Box::new(AnyByte))))),
+//         parse(vec![OpenRound, OpenRound, Dot, CloseRound, CloseRound])
+//     );
+//     assert_eq!(
+//         Ok(Group(Box::new(Seq(vec![
+//             AnyByte,
+//             Group(Box::new(AnyByte))
+//         ])))),
+//         parse(vec![OpenRound, Dot, OpenRound, Dot, CloseRound, CloseRound])
+//     );
+// }
+//
+// #[cfg(test)]
+// #[test]
+// fn test_parse_repeat() {
+//     use Node::{AnyByte, Repeat};
+//     use Token::{Byte, CloseCurly, Dot, OpenCurly, Plus, QMark, Star};
+//     // ?
+//     assert_eq!(
+//         Err("missing element before `?` symbol".to_string()),
+//         parse(vec![QMark])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, Some(1))),
+//         parse(vec![Dot, QMark])
+//     );
+//
+//     // *
+//     assert_eq!(
+//         Err("missing element before `*` symbol".to_string()),
+//         parse(vec![Star])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, None)),
+//         parse(vec![Dot, Star])
+//     );
+//
+//     // +
+//     assert_eq!(
+//         Err("missing element before `+` symbol".to_string()),
+//         parse(vec![Plus])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 1, None)),
+//         parse(vec![Dot, Plus])
+//     );
+//
+//     // {1}
+//     assert_eq!(
+//         Err("missing element before `{` symbol".to_string()),
+//         parse(vec![OpenCurly, Byte(b'1'), CloseCurly])
+//     );
+//     assert_eq!(
+//         Err("missing `}` symbol".to_string()),
+//         parse(vec![Dot, OpenCurly, Byte(b'1')])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, Some(0))),
+//         parse(vec![Dot, OpenCurly, Byte(b'0'), CloseCurly])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 1, Some(1))),
+//         parse(vec![Dot, OpenCurly, Byte(b'1'), CloseCurly])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 99, Some(99))),
+//         parse(vec![Dot, OpenCurly, Byte(b'9'), Byte(b'9'), CloseCurly])
+//     );
+//
+//     // {,}
+//     assert_eq!(
+//         Err("missing element before `{` symbol".to_string()),
+//         parse(vec![OpenCurly, Byte(b','), CloseCurly])
+//     );
+//     assert_eq!(
+//         Err("missing `}` symbol".to_string()),
+//         parse(vec![Dot, OpenCurly, Byte(b',')])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, None)),
+//         parse(vec![Dot, OpenCurly, Byte(b','), CloseCurly])
+//     );
+//
+//     // {1,}
+//     assert_eq!(
+//         Err("missing element before `{` symbol".to_string()),
+//         parse(vec![OpenCurly, Byte(b'1'), Byte(b','), CloseCurly])
+//     );
+//     assert_eq!(
+//         Err("missing `}` symbol".to_string()),
+//         parse(vec![Dot, OpenCurly, Byte(b'1'), Byte(b',')])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, None)),
+//         parse(vec![Dot, OpenCurly, Byte(b'0'), Byte(b','), CloseCurly])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 1, None)),
+//         parse(vec![Dot, OpenCurly, Byte(b'1'), Byte(b','), CloseCurly])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 99, None)),
+//         parse(vec![
+//             Dot,
+//             OpenCurly,
+//             Byte(b'9'),
+//             Byte(b'9'),
+//             Byte(b','),
+//             CloseCurly
+//         ])
+//     );
+//
+//     // {,1}
+//     assert_eq!(
+//         Err("missing element before `{` symbol".to_string()),
+//         parse(vec![OpenCurly, Byte(b','), Byte(b'1'), CloseCurly])
+//     );
+//     assert_eq!(
+//         Err("missing `}` symbol".to_string()),
+//         parse(vec![Dot, OpenCurly, Byte(b','), Byte(b'1')])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, Some(0))),
+//         parse(vec![Dot, OpenCurly, Byte(b','), Byte(b'0'), CloseCurly])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, Some(1))),
+//         parse(vec![Dot, OpenCurly, Byte(b','), Byte(b'1'), CloseCurly])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, Some(99))),
+//         parse(vec![
+//             Dot,
+//             OpenCurly,
+//             Byte(b','),
+//             Byte(b'9'),
+//             Byte(b'9'),
+//             CloseCurly
+//         ])
+//     );
+//
+//     // {1,2}
+//     assert_eq!(
+//         Err("missing element before `{` symbol".to_string()),
+//         parse(vec![
+//             OpenCurly,
+//             Byte(b'1'),
+//             Byte(b','),
+//             Byte(b'2'),
+//             CloseCurly
+//         ])
+//     );
+//     assert_eq!(
+//         Err("missing `}` symbol".to_string()),
+//         parse(vec![Dot, OpenCurly, Byte(b'1'), Byte(b','), Byte(b'2')])
+//     );
+//     assert_eq!(
+//         Err("repeating element has max that is smaller than min: {2,1}".to_string()),
+//         parse(vec![Dot, OpenCurly, Byte(b'2'), Byte(b','), Byte(b'1')])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 0, Some(0))),
+//         parse(vec![
+//             Dot,
+//             OpenCurly,
+//             Byte(b'0'),
+//             Byte(b','),
+//             Byte(b'0'),
+//             CloseCurly
+//         ])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 1, Some(2))),
+//         parse(vec![
+//             Dot,
+//             OpenCurly,
+//             Byte(b'1'),
+//             Byte(b','),
+//             Byte(b'2'),
+//             CloseCurly
+//         ])
+//     );
+//     assert_eq!(
+//         Ok(Repeat(Box::new(AnyByte), 10, Some(99))),
+//         parse(vec![
+//             Dot,
+//             OpenCurly,
+//             Byte(b'1'),
+//             Byte(b'0'),
+//             Byte(b','),
+//             Byte(b'9'),
+//             Byte(b'9'),
+//             CloseCurly
+//         ])
+//     );
+// }
 
 fn impl_regex(stream: TokenStream) -> Result<TokenStream, String> {
     // Literal { kind: ByteStrRaw(0), symbol: "abc\\x20", suffix: None, span: #0 bytes(741..752) }
