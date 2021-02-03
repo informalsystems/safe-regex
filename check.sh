ff06e3abde55f7e43650b8a46aa28ebfe7c57153
Michael Leonhard <michael@leonhardllc.com>
2021-02-03 15:59:07 -0800

    CI to run check-all.sh

--- check.sh
+++ check.sh
@@ -5,7 +5,6 @@
 
 check_cargo_readme() {
   cargo readme >Readme.md.tmp
-  ls -alF "$CI_PROJECT_DIR" || true
   # Once cargo-geiger-serde builds on nightly,
   # change this to always run `cargo geiger`.
   # https://github.com/rust-secure-code/cargo-geiger/issues/181
@@ -21,22 +20,14 @@ check_cargo_readme() {
 }
 
 check() {
-  ls -alF "$CI_PROJECT_DIR" || true
   time cargo check --verbose
-  ls -alF "$CI_PROJECT_DIR" || true
   time cargo build --verbose
-  ls -alF "$CI_PROJECT_DIR" || true
   time cargo test --verbose
-  ls -alF "$CI_PROJECT_DIR" || true
   time cargo fmt --all -- --check
-  ls -alF "$CI_PROJECT_DIR" || true
   time cargo clippy --all-targets --all-features -- -D clippy::pedantic
-  ls -alF "$CI_PROJECT_DIR" || true
   #time check_cargo_readme
-  ls -alF "$CI_PROJECT_DIR" || true
   time cargo publish --dry-run "$@"
   echo "$0 finished"
-  ls -alF "$CI_PROJECT_DIR" || true
 }
 set -e
 set -x
