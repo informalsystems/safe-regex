fcddc4c17a48987618f0dc13a95a69dd64d0f955
Michael Leonhard <michael@leonhardllc.com>
2021-02-28 01:26:40 -0800

    Rework benchmarks

--- bench/benches/lib.rs
+++ bench/benches/lib.rs
@@ -1,31 +1,40 @@
-// $ cargo +nightly bench
-// test byte_regex                      ... bench:          25 ns/iter (+/- 1)
-// test byte_safe_regex                 ... bench:         141 ns/iter (+/- 7)
-// test capture10_regex                 ... bench:       2,290 ns/iter (+/- 554)
-// test capture10_safe_regex            ... bench:   2,020,367 ns/iter (+/- 83,701)
-// test datetime_capture_1kb_regex      ... bench:       1,085 ns/iter (+/- 129)
-// test datetime_capture_1kb_safe_regex ... bench:     160,590 ns/iter (+/- 8,890)
-// test datetime_capture_50_regex       ... bench:         197 ns/iter (+/- 25)
-// test datetime_capture_50_safe_regex  ... bench:      12,111 ns/iter (+/- 1,887)
-// test pem_base64_regex                ... bench:         119 ns/iter (+/- 12)
-// test pem_base64_safe_regex           ... bench:      42,833 ns/iter (+/- 5,893)
-// test phone_capture_100kb_regex       ... bench:     178,807 ns/iter (+/- 9,806)
-// test phone_capture_100kb_safe_regex  ... bench:  18,234,422 ns/iter (+/- 756,328)
-// test repeat10_regex                  ... bench:          74 ns/iter (+/- 4)
-// test repeat10_safe_regex             ... bench:       7,153 ns/iter (+/- 1,261)
-// test repeat30_regex                  ... bench:         141 ns/iter (+/- 32)
-// test repeat_capture10_regex          ... bench:       1,408 ns/iter (+/- 199)
-// test repeat_capture10_safe_regex     ... bench:      10,900 ns/iter (+/- 1,165)
-// test repeat_capture20_regex          ... bench:       4,468 ns/iter (+/- 687)
-// test repeat_capture20_safe_regex     ... bench:      37,770 ns/iter (+/- 5,292)
-// test substring1_regex                ... bench:          10 ns/iter (+/- 1)
-// test substring1_safe_regex           ... bench:         529 ns/iter (+/- 80)
-// test substring50_regex               ... bench:          28 ns/iter (+/- 2)
-// test substring50_safe_regex          ... bench:       6,497 ns/iter (+/- 340)
-// test substring_100kb_regex           ... bench:       1,079 ns/iter (+/- 51)
-// test substring_100kb_safe_regex      ... bench:  12,230,604 ns/iter (+/- 805,527)
-// test substring_1kb_regex             ... bench:          21 ns/iter (+/- 3)
-// test substring_1kb_safe_regex        ... bench:     122,942 ns/iter (+/- 10,317)
+// $ cargo +nightly bench --package bench
+//     Finished bench [optimized] target(s) in 9.11s
+//      Running target/release/deps/lib-9f5f0d3bacb64d1e
+//
+// running 30 tests
+// test capture10_regex                  ... bench:       2,324 ns/iter (+/- 356)
+// test capture10_safe_regex             ... bench:   1,829,491 ns/iter (+/- 183,937)
+// test datetime_capture_100_regex       ... bench:         217 ns/iter (+/- 20)
+// test datetime_capture_100_safe_regex  ... bench:      23,064 ns/iter (+/- 3,036)
+// test datetime_capture_10kb_regex      ... bench:      12,152 ns/iter (+/- 2,820)
+// test datetime_capture_10kb_safe_regex ... bench:   2,212,062 ns/iter (+/- 201,341)
+// test datetime_capture_1kb_regex       ... bench:       1,145 ns/iter (+/- 102)
+// test datetime_capture_1kb_safe_regex  ... bench:     233,475 ns/iter (+/- 27,248)
+// test datetime_parse_regex             ... bench:         401 ns/iter (+/- 82)
+// test datetime_parse_safe_regex        ... bench:      11,441 ns/iter (+/- 15,730)
+// test pem_base64_regex                 ... bench:         116 ns/iter (+/- 7)
+// test pem_base64_safe_regex            ... bench:   1,022,333 ns/iter (+/- 79,905)
+// test phone_capture_100kb_regex        ... bench:     185,383 ns/iter (+/- 12,965)
+// test phone_capture_100kb_safe_regex   ... bench:  17,014,889 ns/iter (+/- 1,540,627)
+// test phone_capture_10kb_regex         ... bench:      17,555 ns/iter (+/- 2,999)
+// test phone_capture_10kb_safe_regex    ... bench:   1,712,844 ns/iter (+/- 299,877)
+// test phone_capture_1kb_regex          ... bench:       2,058 ns/iter (+/- 446)
+// test phone_capture_1kb_safe_regex     ... bench:     163,661 ns/iter (+/- 11,667)
+// test repeat10_regex                   ... bench:          72 ns/iter (+/- 7)
+// test repeat10_safe_regex              ... bench:       9,898 ns/iter (+/- 810)
+// test repeat_capture10_regex           ... bench:       1,413 ns/iter (+/- 415)
+// test repeat_capture10_safe_regex      ... bench:      15,337 ns/iter (+/- 1,822)
+// test string_search_100_regex          ... bench:          22 ns/iter (+/- 3)
+// test string_search_100_safe_regex     ... bench:      11,415 ns/iter (+/- 1,552)
+// test string_search_100kb_regex        ... bench:       1,243 ns/iter (+/- 148)
+// test string_search_100kb_safe_regex   ... bench:  12,103,077 ns/iter (+/- 993,985)
+// test string_search_10kb_regex         ... bench:          84 ns/iter (+/- 8)
+// test string_search_10kb_safe_regex    ... bench:   1,182,229 ns/iter (+/- 116,326)
+// test string_search_1kb_regex          ... bench:          22 ns/iter (+/- 5)
+// test string_search_1kb_safe_regex     ... bench:     116,343 ns/iter (+/- 12,469)
+//
+// test result: ok. 0 passed; 0 failed; 0 ignored; 30 measured; 0 filtered out; finished in 77.27s
 #![allow(soft_unstable)]
 #![feature(test)]
 #![forbid(unsafe_code)]
@@ -34,119 +43,70 @@ use regex::bytes::Regex;
 use safe_regex::{regex, Matcher};
 use test::Bencher;
 
-const FIFTY_BYTES: &[u8] = b"H9JTLVCN8Z5FLRGH1T8JDX4QKPNP5CKN4M6PZ139W9JJVTC8K9";
-const PEM_BASE64_LINE: &[u8] = b"psGUNwWXrARgiInCeQkvN3toQrXOyQ5Df3MwrTAUIy0Nec7MrUEcdjrE0Mks3HhH";
-
-fn rand_bytes(n: usize) -> Vec<u8> {
+fn rand_bytes_without_z(n: usize) -> Vec<u8> {
     core::iter::from_fn(|| Some(rand::random()))
-        .filter(|b| *b != b'Z')
+        .filter(|b| *b != b'Z' && *b != b'z')
         .take(n)
         .collect()
 }
 
 #[bench]
-fn byte_regex(b: &mut Bencher) {
-    let re = Regex::new(r"^a$").unwrap();
-    b.iter(|| re.is_match(b"X"));
-}
-
-#[bench]
-fn byte_safe_regex(b: &mut Bencher) {
-    let re: Matcher<_> = regex!(br"a");
-    b.iter(|| re.match_all(b"X"));
-}
-
-#[bench]
-fn substring1_regex(b: &mut Bencher) {
+fn string_search_100_regex(b: &mut Bencher) {
     let re = Regex::new(r"2G8H81RFNZ").unwrap();
-    b.iter(|| re.find(b"X"));
-}
-
-#[bench]
-fn substring1_safe_regex(b: &mut Bencher) {
-    let re: Matcher<_> = regex!(br".*(2G8H81RFNZ).*");
-    b.iter(|| re.match_all(b"X"));
-}
-
-#[bench]
-fn substring50_regex(b: &mut Bencher) {
-    let re = Regex::new(r"2G8H81RFNZ").unwrap();
-    b.iter(|| re.find(FIFTY_BYTES));
-}
-
-#[bench]
-fn substring50_safe_regex(b: &mut Bencher) {
-    let re: Matcher<_> = regex!(br".*(2G8H81RFNZ).*");
-    b.iter(|| re.match_all(FIFTY_BYTES));
-}
-
-#[bench]
-fn substring_1kb_regex(b: &mut Bencher) {
-    let re = Regex::new(r"2G8H81RFNZ").unwrap();
-    let data: Vec<u8> = rand_bytes(1024);
+    let data: Vec<u8> = rand_bytes_without_z(1024);
     b.iter(|| re.find(&data));
 }
 
 #[bench]
-fn substring_1kb_safe_regex(b: &mut Bencher) {
+fn string_search_100_safe_regex(b: &mut Bencher) {
     let re: Matcher<_> = regex!(br".*(2G8H81RFNZ).*");
-    let data: Vec<u8> = rand_bytes(1024);
+    let data: Vec<u8> = rand_bytes_without_z(100);
     b.iter(|| re.match_all(&data));
 }
 
 #[bench]
-fn substring_100kb_regex(b: &mut Bencher) {
+fn string_search_1kb_regex(b: &mut Bencher) {
     let re = Regex::new(r"2G8H81RFNZ").unwrap();
-    let data: Vec<u8> = rand_bytes(100 * 1024);
+    let data: Vec<u8> = rand_bytes_without_z(1024);
     b.iter(|| re.find(&data));
 }
 
 #[bench]
-fn substring_100kb_safe_regex(b: &mut Bencher) {
+fn string_search_1kb_safe_regex(b: &mut Bencher) {
     let re: Matcher<_> = regex!(br".*(2G8H81RFNZ).*");
-    let data: Vec<u8> = rand_bytes(100 * 1024);
+    let data: Vec<u8> = rand_bytes_without_z(1024);
     b.iter(|| re.match_all(&data));
 }
 
-// #[bench]
-// fn substring_1mb_regex(b: &mut Bencher) {
-//     let re = Regex::new(r"2G8H81RFNZ").unwrap();
-//     let data: Vec<u8> = rand_bytes(1024 * 1024);
-//     b.iter(|| re.find(&data));
-// }
-//
-// #[bench]
-// fn substring_1mb_safe_regex(b: &mut Bencher) {
-//     let re: Matcher<_> = regex!(br".*(2G8H81RFNZ).*");
-//     let data: Vec<u8> = rand_bytes(1024 * 1024);
-//     b.iter(|| re.match_all(&data));
-// }
-
-// #[bench]
-// fn substring_1gb_regex(b: &mut Bencher) {
-//     let re = Regex::new(r"2G8H81RFNZ").unwrap();
-//     let data: Vec<u8> = rand_bytes(1024 * 1024 * 1024);
-//     b.iter(|| re.find(&data));
-// }
-//
-// #[bench]
-// fn substring_1gb_safe_regex(b: &mut Bencher) {
-//     let re: Matcher<_> = regex!(br".*(2G8H81RFNZ).*");
-//     let data: Vec<u8> = rand_bytes(1024 * 1024 * 1024);
-//     b.iter(|| re.match_all(&data));
-// }
-
 #[bench]
-fn pem_base64_regex(b: &mut Bencher) {
-    let re = Regex::new(r"^[a-zA-Z0-9+/=]{4}{0,16}$").unwrap();
-    b.iter(|| re.is_match(PEM_BASE64_LINE));
+fn string_search_10kb_regex(b: &mut Bencher) {
+    let re = Regex::new(r"2G8H81RFNZ").unwrap();
+    let data: Vec<u8> = rand_bytes_without_z(10 * 1024);
+    b.iter(|| re.find(&data));
 }
 
 #[bench]
-fn pem_base64_safe_regex(b: &mut Bencher) {
-    let re: Matcher<_> = regex!(br"[a-zA-Z0-9+/=]{4}{0,16}");
-    b.iter(|| re.match_all(PEM_BASE64_LINE));
+fn string_search_10kb_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br".*(2G8H81RFNZ).*");
+    let data: Vec<u8> = rand_bytes_without_z(10 * 1024);
+    b.iter(|| re.match_all(&data));
 }
+
+#[bench]
+fn string_search_100kb_regex(b: &mut Bencher) {
+    let re = Regex::new(r"2G8H81RFNZ").unwrap();
+    let data: Vec<u8> = rand_bytes_without_z(100 * 1024);
+    b.iter(|| re.find(&data));
+}
+
+#[bench]
+fn string_search_100kb_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br".*(2G8H81RFNZ).*");
+    let data: Vec<u8> = rand_bytes_without_z(100 * 1024);
+    b.iter(|| re.match_all(&data));
+}
+
+//////////////
 
 #[bench]
 fn repeat10_regex(b: &mut Bencher) {
@@ -160,6 +120,33 @@ fn repeat10_safe_regex(b: &mut Bencher) {
     b.iter(|| re.match_all(b"aaaaaaaaaa"));
 }
 
+// #[bench]
+// fn repeat20_regex(b: &mut Bencher) {
+//     let re = Regex::new(r"^a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaa$").unwrap();
+//     b.iter(|| re.captures(b"aaaaaaaaaaaaaaaaaaaa"));
+// }
+//
+// // Very very slow.  May never complete.
+// #[bench]
+// fn repeat20_safe_regex(b: &mut Bencher) {
+//     let re: Matcher<_> = regex!(br"a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaa");
+//     b.iter(|| re.match_all(b"aaaaaaaaaaaaaaaaaaaa"));
+// }
+//
+// #[bench]
+// fn repeat30_safe_regex(b: &mut Bencher) {
+//     let re: Matcher<_> = regex!(br"a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+//     b.iter(|| re.match_all(b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+// }
+//
+// #[bench]
+// fn repeat30_regex(b: &mut Bencher) {
+//     let re = Regex::new(r"^a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa$").unwrap();
+//     b.iter(|| re.is_match(b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+// }
+
+//////////////
+
 #[bench]
 fn repeat_capture10_regex(b: &mut Bencher) {
     let re = Regex::new(r"^(a?a?a?a?a?a?a?a?a?a?aaaaaaaaaa)$").unwrap();
@@ -172,31 +159,20 @@ fn repeat_capture10_safe_regex(b: &mut Bencher) {
     b.iter(|| re.match_all(b"aaaaaaaaaa"));
 }
 
-#[bench]
-fn repeat_capture20_regex(b: &mut Bencher) {
-    let re =
-        Regex::new(r"^(a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaa)$").unwrap();
-    b.iter(|| re.captures(b"aaaaaaaaaaaaaaaaaaaa"));
-}
-
-#[bench]
-fn repeat_capture20_safe_regex(b: &mut Bencher) {
-    let re: Matcher<_> = regex!(br"(a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaa)");
-    b.iter(|| re.match_all(b"aaaaaaaaaaaaaaaaaaaa"));
-}
-
-// Hangs rustc.
 // #[bench]
-// fn repeat30_safe_regex(b: &mut Bencher) {
-//     let re: Matcher<_> = regex!(br"a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-//     b.iter(|| re.match_all(b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+// fn repeat_capture20_regex(b: &mut Bencher) {
+//     let re =
+//         Regex::new(r"^(a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaa)$").unwrap();
+//     b.iter(|| re.captures(b"aaaaaaaaaaaaaaaaaaaa"));
+// }
+//
+// #[bench]
+// fn repeat_capture20_safe_regex(b: &mut Bencher) {
+//     let re: Matcher<_> = regex!(br"(a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaa)");
+//     b.iter(|| re.match_all(b"aaaaaaaaaaaaaaaaaaaa"));
 // }
 
-#[bench]
-fn repeat30_regex(b: &mut Bencher) {
-    let re = Regex::new(r"^a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa$").unwrap();
-    b.iter(|| re.is_match(b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
-}
+//////////////
 
 #[bench]
 fn capture10_regex(b: &mut Bencher) {
@@ -210,56 +186,147 @@ fn capture10_safe_regex(b: &mut Bencher) {
     b.iter(|| re.match_all(b"aaaaaaaaaa"));
 }
 
+// #[bench]
+// fn capture20_regex(b: &mut Bencher) {
+//     let re = Regex::new(r"^(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)aaaaaaaaaaaaaaaaaaaa$").unwrap();
+//     b.iter(|| re.captures(b"aaaaaaaaaaaaaaaaaaaa"));
+// }
+//
+// #[bench]
+// fn capture20_safe_regex(b: &mut Bencher) {
+//     let re: Matcher<_> = regex!(br"(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)(a?)aaaaaaaaaaaaaaaaaaaa");
+//     b.iter(|| re.match_all(b"aaaaaaaaaaaaaaaaaaaa"));
+// }
+
+//////////////
+
 #[bench]
-fn datetime_capture_50_safe_regex(b: &mut Bencher) {
-    let re: Matcher<_> = regex!(br".*([0-9]+-[0-9]+-[0-9]+) ([0-9]+:[0-9]+).*");
-    b.iter(|| re.match_all(FIFTY_BYTES));
+fn datetime_capture_100_regex(b: &mut Bencher) {
+    let re = Regex::new(r"([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)").unwrap();
+    let data: Vec<u8> = rand_bytes_without_z(100);
+    b.iter(|| re.captures(&data));
 }
 
 #[bench]
-fn datetime_capture_50_regex(b: &mut Bencher) {
-    let re = Regex::new(r"([0-9]+-[0-9]+-[0-9]+) ([0-9]+:[0-9]+)").unwrap();
-    b.iter(|| re.captures(FIFTY_BYTES));
-}
-
-#[bench]
-fn datetime_capture_1kb_safe_regex(b: &mut Bencher) {
-    let re: Matcher<_> = regex!(br".*([0-9]+-[0-9]+-[0-9]+) ([0-9]+:[0-9]+).*");
-    let data: Vec<u8> = rand_bytes(1024);
+fn datetime_capture_100_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br".*([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+).*");
+    let data: Vec<u8> = rand_bytes_without_z(100);
     b.iter(|| re.match_all(&data));
 }
 
 #[bench]
 fn datetime_capture_1kb_regex(b: &mut Bencher) {
-    let re = Regex::new(r"([0-9]+-[0-9]+-[0-9]+) ([0-9]+:[0-9]+)").unwrap();
-    let data: Vec<u8> = rand_bytes(1024);
+    let re = Regex::new(r"([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)").unwrap();
+    let data: Vec<u8> = rand_bytes_without_z(1024);
+    b.iter(|| re.captures(&data));
+}
+
+#[bench]
+fn datetime_capture_1kb_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br".*([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+).*");
+    let data: Vec<u8> = rand_bytes_without_z(1024);
+    b.iter(|| re.match_all(&data));
+}
+
+#[bench]
+fn datetime_capture_10kb_regex(b: &mut Bencher) {
+    let re = Regex::new(r"([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)").unwrap();
+    let data: Vec<u8> = rand_bytes_without_z(10 * 1024);
+    b.iter(|| re.captures(&data));
+}
+
+#[bench]
+fn datetime_capture_10kb_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br".*([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+).*");
+    let data: Vec<u8> = rand_bytes_without_z(10 * 1024);
+    b.iter(|| re.match_all(&data));
+}
+
+//////////////
+
+const DATE_TIME: &[u8] = b"1999-12-32 23:59";
+#[bench]
+fn datetime_parse_regex(b: &mut Bencher) {
+    let re = Regex::new(r"^([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)$").unwrap();
+    b.iter(|| re.captures(DATE_TIME));
+}
+
+#[bench]
+fn datetime_parse_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br"([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+)");
+    b.iter(|| re.match_all(DATE_TIME));
+}
+
+//////////////
+
+const PEM_BASE64_LINE: &[u8] = b"psGUNwWXrARgiInCeQkvN3toQrXOyQ5Df3MwrTAUIy0Nec7MrUEcdjrE0Mks3HhH";
+
+#[bench]
+fn pem_base64_regex(b: &mut Bencher) {
+    let re = Regex::new(r"^[a-zA-Z0-9+/]{0,64}=*$").unwrap();
+    b.iter(|| re.is_match(PEM_BASE64_LINE));
+}
+
+#[bench]
+fn pem_base64_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br"[a-zA-Z0-9+/=]{0,64}=*");
+    b.iter(|| re.match_all(PEM_BASE64_LINE));
+}
+
+//////////////
+
+#[bench]
+fn phone_capture_1kb_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br".*([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4}).*");
+    let data: Vec<u8> = rand_bytes_without_z(1024);
+    b.iter(|| re.match_all(&data));
+}
+
+#[bench]
+fn phone_capture_1kb_regex(b: &mut Bencher) {
+    let re = Regex::new(r"([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4})").unwrap();
+    let data: Vec<u8> = rand_bytes_without_z(1024);
+    b.iter(|| re.captures(&data));
+}
+
+#[bench]
+fn phone_capture_10kb_safe_regex(b: &mut Bencher) {
+    let re: Matcher<_> = regex!(br".*([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4}).*");
+    let data: Vec<u8> = rand_bytes_without_z(10 * 1024);
+    b.iter(|| re.match_all(&data));
+}
+
+#[bench]
+fn phone_capture_10kb_regex(b: &mut Bencher) {
+    let re = Regex::new(r"([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4})").unwrap();
+    let data: Vec<u8> = rand_bytes_without_z(10 * 1024);
     b.iter(|| re.captures(&data));
 }
 
 #[bench]
 fn phone_capture_100kb_safe_regex(b: &mut Bencher) {
     let re: Matcher<_> = regex!(br".*([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4}).*");
-    let data: Vec<u8> = rand_bytes(100 * 1024);
+    let data: Vec<u8> = rand_bytes_without_z(100 * 1024);
     b.iter(|| re.match_all(&data));
 }
 
 #[bench]
 fn phone_capture_100kb_regex(b: &mut Bencher) {
     let re = Regex::new(r"([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4})").unwrap();
-    let data: Vec<u8> = rand_bytes(100 * 1024);
+    let data: Vec<u8> = rand_bytes_without_z(100 * 1024);
     b.iter(|| re.captures(&data));
 }
 
 // #[bench]
 // fn phone_capture_1mb_safe_regex(b: &mut Bencher) {
 //     let re: Matcher<_> = regex!(br".*([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4}).*");
-//     let data: Vec<u8> = rand_bytes(1024 * 1024);
+//     let data: Vec<u8> = rand_bytes_without_z(1024 * 1024);
 //     b.iter(|| re.match_all(&data));
 // }
 //
 // #[bench]
 // fn phone_capture_1mb_regex(b: &mut Bencher) {
 //     let re = Regex::new(r"([0-9]{3})[-. ]?([0-9]{3})[-. ]?([0-9]{4})").unwrap();
-//     let data: Vec<u8> = rand_bytes(1024 * 1024);
+//     let data: Vec<u8> = rand_bytes_without_z(1024 * 1024);
 //     b.iter(|| re.captures(&data));
 // }
